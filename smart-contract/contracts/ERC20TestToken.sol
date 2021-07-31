fc3d913df79777e7d43f4ae39a907a1a12acfad3
Thomas Eapen <thomas.eapen@yahoo.co.in>
2021-07-31 01:09:14 -0400

    Simple ERC20 Smart Contract with Safe Math functions

---
+++ smart-contract/contracts/ERC20TestToken.sol
@@ -0,0 +1,69 @@
+pragma solidity 0.8.6;
+
+import './IERC20.sol';
+import './SafeMath.sol';
+
+contract ERC20TestToken is IERC20 {
+
+
+    string public constant name = "ERCTestToken";
+
+    string public constant symbol = "ERCTEST";
+
+    
+
+
+    mapping(address => uint256) balances;
+
+
+    mapping(address => mapping (address => uint256)) allowed;
+
+
+    uint256 totalSupply_ = 10 ether;
+
+
+    using SafeMath for uint256;
+
+
+    constructor() public payable{
+
+    balances[msg.sender] = totalSupply_;
+
+    }
+
+
+    function totalSupply() public override view returns (uint256) {
+
+    return totalSupply_;
+
+    }
+
+
+    function balanceOf(address tokenOwner) public override view returns (uint256) {
+
+        return balances[tokenOwner];
+
+    }
+
+
+    function transfer(address receiver, uint256 numTokens) public  override returns (bool) {
+
+        require(numTokens <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+
+        balances[receiver] = balances[receiver].add(numTokens);
+
+        return true;
+
+    }
+    
+    
+
+
+    
+
+
+    
+
+}
---
+++ smart-contract/contracts/IERC20.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.6;
+
+interface IERC20 {
+
+
+    function totalSupply() external view  returns (uint256);
+
+    function balanceOf(address account) external view   returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external   returns (bool);
+
+   
+}
