ab230667620474f9c529c107eb3c9161586a1f6b
Thomas Eapen <thomas.eapen@yahoo.co.in>
2021-08-01 21:49:32 -0400

    Cleaning up files

--- smart-contract/contracts/IERC721.sol
+++ smart-contract/contracts/IERC721.sol
@@ -2,28 +2,11 @@ pragma solidity 0.8.6 ;
 
 /**
  * @dev ERC-721 non-fungible token standard.
- * See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md.
+
  */
 interface IERC721
 {
 
- 
-
-
-
-
-
-
-  /**
-   * @dev Throws unless `msg.sender` is the current owner, an authorized operator, or the approved
-   * address for this NFT. Throws if `_from` is not the current owner. Throws if `_to` is the zero
-   * address. Throws if `_tokenId` is not a valid NFT.
-   * @notice The caller is responsible to confirm that `_to` is capable of receiving NFTs or else
-   * they may be permanently lost.
-   * @param _from The current owner of the NFT.
-   * @param _to The new owner.
-   * @param _tokenId The NFT to transfer.
-   */
   function transferFrom(
     address _from,
     address _to,
@@ -32,13 +15,7 @@ interface IERC721
     external;
 
   
-  /**
-   * @dev Returns the number of NFTs owned by `_owner`. NFTs assigned to the zero address are
-   * considered invalid, and this function throws for queries about the zero address.
-   * @notice Count all NFTs assigned to an owner.
-   * @param _owner Address for whom to query the balance.
-   * @return Balance of _owner.
-   */
+  
   function balanceOf(
     address _owner
   )
@@ -46,13 +23,7 @@ interface IERC721
     view
     returns (uint256);
 
-  /**
-   * @dev Returns the address of the owner of the NFT. NFTs assigned to the zero address are
-   * considered invalid, and queries about them do throw.
-   * @notice Find the owner of an NFT.
-   * @param _tokenId The identifier for an NFT.
-   * @return Address of _tokenId owner.
-   */
+ 
   function ownerOf(
     uint256 _tokenId
   )
